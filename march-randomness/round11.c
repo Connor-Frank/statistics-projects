123d10074837eda214da6a8b8a39e0b9361bdfa4
Connor Frank <connorjfrank@gmail.com>
2021-04-09 18:21:37 -0400

    round 11

---
+++ march-randomness/round11.c
@@ -0,0 +1,76 @@
+/*
+ * THE EXPERIMENT
+ *
+ * Roll a 10-sided die, then roll a 20-sided die,
+ * then roll a 30-sided die, keeping track of each result in order.
+ *
+ * Will the results be in strictly increasing order?
+ */
+
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+bool consecutive(int a, int b, int c);
+void seed_rand(void);
+int int_rand(int min, int max);
+
+int main(void) {
+  seed_rand();
+
+  long times_increasing = 0;
+  long trials = (long)1e+8; // takes about 2 seconds
+
+  clock_t start = clock();
+
+  int ten_sided, twenty_sided, thirty_sided;
+
+  for (long i = 0; i < trials; i++) {
+    ten_sided = int_rand(1, 10);
+    twenty_sided = int_rand(1, 20);
+    thirty_sided = int_rand(1, 30);
+    if (consecutive(ten_sided, twenty_sided, thirty_sided)) {
+      times_increasing++;
+    }
+  }
+
+  clock_t end = clock();
+  double duration = ((double)(end - start)) / CLOCKS_PER_SEC;
+
+  printf("%ld trials were simulated.\n", trials);
+  printf("the results were in strictly increasing order %ld times.\n",
+         times_increasing);
+  printf("that's equal to %Lf%% of the time\n",
+         (long double)times_increasing / trials * 100.0);
+  printf("time to run: %f seconds\n", duration);
+
+  return 0;
+}
+
+bool consecutive(int a, int b, int c) {
+  switch (abs(c - a)) {
+  case 2:
+    return (2 * b == a + c);
+  case 1:
+    return consecutive(b, c, a);
+  default:
+    return false;
+  }
+}
+
+void seed_rand(void) {
+  FILE *fp;
+  fp = fopen("/dev/random", "r");
+  unsigned long long rand_seed;
+  fread(&rand_seed, sizeof(rand_seed), sizeof(char), fp);
+  fclose(fp);
+  printf("random seed: %llu\n", rand_seed);
+  srand((unsigned int)rand_seed);
+}
+
+int int_rand(int min, int max) {
+  if (min >= max)
+    printf("you messed up.\n");
+  return (rand() % (max - min + 1)) + min;
+}
