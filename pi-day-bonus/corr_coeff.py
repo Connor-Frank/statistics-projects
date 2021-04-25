d67067281e23f7d9a9a8a91bdd3ef74af440dd08
Connor Frank <connorjfrank@gmail.com>
2021-04-25 14:27:17 -0400

    pi day cleanup

--- pi-day-bonus/corr_coeff.py
+++ pi-day-bonus/corr_coeff.py
@@ -3,15 +3,15 @@ import math
 
 def correlation_coefficient(x, y):
     n = len(x)
-    sum_x = sum_y = sum_xy = squaresum_x = squaresum_y = 0
+    sum_x = sum_y = sum_xy = square_sum_x = square_sum_y = 0
 
     for i in range(0, n, 1):
         sum_x += x[i]
         sum_y += y[i]
         sum_xy += x[i] * y[i]
-        squaresum_x += x[i] * x[i]
-        squaresum_y += y[i] * y[i]
+        square_sum_x += x[i] * x[i]
+        square_sum_y += y[i] * y[i]
 
-    return float(n * sum_xy - sum_x * sum_y)/float(math.sqrt((n * squaresum_x -
-                                                              sum_x**2) * (n * squaresum_y -
-                                                                           sum_y**2)))
+    return float(n * sum_xy - sum_x * sum_y) / float(math.sqrt((n * square_sum_x -
+                                                                sum_x ** 2) * (n * square_sum_y -
+                                                                               sum_y ** 2)))
